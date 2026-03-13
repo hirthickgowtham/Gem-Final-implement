884058d4ed2f683f492bb46663d3fdc6cf53cdde
vijay <vijayalayank26@gmai.com>
2026-03-13 10:43:13 +0530

    added .js

--- ApiGateWay/src/load/cors.load.js
+++ ApiGateWay/src/load/cors.load.js
@@ -1,5 +1,5 @@
 import cors from "cors";
-import corsConfig from "../config/cors.config";
+import corsConfig from "../config/cors.config.js";
 
 export default function loadcors(app){
     app.use(cors(corsConfig)),
