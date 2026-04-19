8189290e6610fadcb1097a040a87894b51df7c4b
vijay <vijayalayank26@gmai.com>
2026-04-20 01:40:12 +0530

    lot number issue fixed

--- MicroService_1/src/middleware/GemDetailEdit/EachGemDetailUpdataChecker.middleware.js
+++ MicroService_1/src/middleware/GemDetailEdit/EachGemDetailUpdataChecker.middleware.js
@@ -1,6 +1,6 @@
 const { sendResponse } = require("../../utils/Response");
 const GemIdCheck = require("../../utils/ExistCheck/GemIdCheck");
-const {LotNumberCheck} = require("../../utils/ExistCheck/LotNumberCheck");
+const {EachGemIdCheck} = require("../../utils/ExistCheck/EachGemIdCheck");
 const ColorIdCheck = require("../../utils/ExistCheck/ColorIdCheck");
 const ShapeIdCheck = require("../../utils/ExistCheck/ShapeIdCheck");
 
@@ -28,12 +28,16 @@ const validationChecker = async (req, res, next) => {
             return sendResponse(res, 400, false, "At least one field must be provided to update: lot_number, crt, number_of_gems, gem_id, category, color_id, shape_id, description");
         }
 
-        if ('lot_number' in gemDetails && !gemDetails.lot_number) {
-            return sendResponse(res, 400, false, "lot_number cannot be empty");
+        if(!gemDetails.each_gem_id){
+            return sendResponse(res, 400, false, "each_gem_id cannot be updated");
         }
 
-        if ('gem_id' in gemDetails && !gemDetails.gem_id) {
-            return sendResponse(res, 400, false, "gem_id cannot be empty");
+        if(gemDetails.each_gem_id){
+            const IsEachGemIdValid = await EachGemIdCheck(gemDetails.each_gem_id);
+
+            if(!IsEachGemIdValid){
+                return sendResponse(res, 400, false, "Invalid each_gem_id provided");
+            }
         }
 
         if(gemDetails.gem_id){
@@ -41,14 +45,6 @@ const validationChecker = async (req, res, next) => {
 
             if(!IsGemIdValid){
                 return sendResponse(res, 400, false, "Invalid gem_id provided");
-            }
-        }
-
-        if(gemDetails.lot_number){
-            const IsLotNumberExist = await LotNumberCheck(gemDetails.lot_number);
-
-            if(!IsLotNumberExist){
-                return sendResponse(res, 400, false, "Invalid lot_number provided");
             }
         }
 
